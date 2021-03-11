f8fc1dc8f2a845db0db28db920d4e7ee057f3cbd
hosam <hosamelmalt@gmail.com>
2021-03-11 12:24:12 +0200

    Initial Commit

---
+++ Stopwatch swiftui/Stopwatch_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stopwatch_swiftuiApp.swift
+//  Stopwatch swiftui
+//
+//  Created by hosam on 3/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Stopwatch_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
